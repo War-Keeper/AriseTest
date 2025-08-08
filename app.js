a124d1fb2f0807ab74750e96db25ff26cea16303
War-Keeper <87688584+War-Keeper@users.noreply.github.com>
2025-08-08 16:44:22 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://unpkg.com/feather-icons"></script>
+<script src="https://cdn.jsdelivr.net/npm/papaparse@5.4.1/papaparse.min.js"></script>
+<script src="app.js"></script>
